1247be30e15a18e5f7676bd865e9f901b8a4e6dc
Ben Galewsky <ben@peartreestudio.net>
2020-02-24 10:06:12 -0600

    Initial project files

---
+++ README.md
@@ -0,0 +1,1 @@
+# ServiceX Command Line Tool
---
+++ setup.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2019, IRIS-HEP
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# setuptools loads some plugins necessary for use here.
+from setuptools import find_packages  # noqa: F401
+from distutils.core import setup
+
+# Use the readme as the long description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name="servicex",
+      version='0.3.rc1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      scripts=[],
+      entry_points={
+          'console_scripts': ['servicex=servicex.servicex:main'],
+      },
+      description="CLI for the ServiceX Data Server",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="Ben Galewsky (IRIS-HEP/NCSA/University of Illinois)",
+      author_email="bengal1@illinois.edu",
+      maintainer="Ben Galewsky (IRIS-HEP/NCSA/University of Illinois)",
+      maintainer_email="bengal1@illinois.edu",
+      url="https://github.com/iris-hep/func_adl_xAOD",
+      license='bsd 3 clause',
+      python_requires='>=3.6',
+      test_suite="tests",
+      install_requires=[
+          "kubernetes"
+      ],
+      extras_require={
+          'test': [
+              'pytest>=3.9',
+              'pytest-mock',
+              'pytest-cov',
+              'coverage',
+              'flake8',
+              'codecov',
+              'autopep8',
+              'twine'
+          ],
+      },
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          # "Development Status :: 4 - Beta",
+          # "Development Status :: 5 - Production/Stable",
+          # "Development Status :: 6 - Mature",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Information Technology",
+          "Programming Language :: Python",
+          "Topic :: Software Development",
+          "Topic :: Utilities",
+      ],
+      platforms="Any",
+      )
