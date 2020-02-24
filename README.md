657a91e75f2395f8b23a9478589ace7d0ecf190b
Ben Galewsky <ben@peartreestudio.net>
2020-02-24 16:30:46 -0600

    Add clear command and README

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # ServiceX Command Line Tool
+This is a tool for setting up the serviceX cluster. 
+
+Its only function for now is to install your grid certs and passphrase
+securely as kubernetes secrets.
+
+## Installation
+The tool is available on pypi so...
+```bash
+pip install servicex-cli
+```
+
+## Usage
+To list the version of the servicex cli installed:
+```bash
+servicex version
+```
+
+### Install Certs
+To install your grid certs
+```bash
+servicex init
+```
+By default, it will find certs in `.globus` in your home directory. You can 
+override this by providing a `--cert-dir` command line option.
+
+By default the secret will be created in the `default` namespace. You can
+override this by providing a `--namespace` command line option.
+
+When the script runs it will prompt you for your grid cert passphrase. This 
+will not be echoed to the screen, but will be stored in the kubernetes secret.
+
+### Remove Certs
+If you want to remove the installed certs and passphrase from the cluster then
+you can use:
+```bash
+servicex clear
+```
+
+It takes the same `--namespace` argument as the `init` command to remove the 
+secret from that namespace.
+ 
+
+
+
--- src/servicex/servicex.py
+++ src/servicex/servicex.py
@@ -26,6 +26,73 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import argparse
+
+import kubernetes
+import pkg_resources
+from kubernetes import client
+import base64
+import os.path
+import os
+import getpass
+
+parser = argparse.ArgumentParser("ServiceX Command Line Interface")
+parser.add_argument("command", choices=["init", "version", "clear"])
+parser.add_argument("--cert-dir",
+                    default="~/.globus",
+                    help="Directory where your grid certs are located")
+parser.add_argument("--namespace", "-n",
+                    default='default',
+                    help="Namespace where secret should be created")
+
+
+def init_cluster(cert_dir, secret_name, namespace):
+    passphrase = getpass.getpass("Enter passphrase for certs: ")
+
+    try:
+        client.CoreV1Api().delete_namespaced_secret(namespace=namespace, name=secret_name)
+        print("Existing secret cleared")
+    except kubernetes.client.rest.ApiException:
+        print("Empty Cluster. Creating secrets for the first time")
+
+    data = {'passphrase': base64.b64encode(passphrase.encode()).decode("ascii")}
+
+    with open(os.path.expanduser(os.path.join(cert_dir, 'usercert.pem')), 'rb') as usercert_file:
+        data['usercert.pem'] = base64.b64encode(usercert_file.read()).decode("ascii")
+
+    with open(os.path.expanduser(os.path.join(cert_dir, 'userkey.pem')), 'rb') as userkey_file:
+        data['userkey.pem'] = base64.b64encode(userkey_file.read()).decode("ascii")
+
+    secret = client.V1Secret(data=data,
+                             kind='Secret',
+                             type='Opaque',
+                             metadata=client.V1ObjectMeta(name=secret_name))
+
+    client.CoreV1Api().create_namespaced_secret(namespace=namespace, body=secret)
+
+
+def clear_cluster(secret_name, namespace):
+    try:
+        client.CoreV1Api().delete_namespaced_secret(namespace=namespace, name=secret_name)
+        print("Secret cleared")
+    except kubernetes.client.rest.ApiException:
+        print("Empty Cluster. Nothing to do....")
+
 
 def main():
-    print("ServiceX!")
+    kubernetes.config.load_kube_config()
+    args = parser.parse_args()
+
+    if args.command == 'init':
+        init_cluster(args.cert_dir, "grid-certs-secret", args.namespace)
+
+    elif args.command == 'clear':
+        clear_cluster("grid-certs-secret", args.namespace)
+
+    elif args.command == 'version':
+        print("ServiceX CLI Version " + pkg_resources.get_distribution('servicex-cli').version)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
